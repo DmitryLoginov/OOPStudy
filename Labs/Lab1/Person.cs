112dde4393d3fb7659c089efa2523cdf21abce22
ldv <daimom25@gmail.com>
2020-12-19 19:55:27 +0700

    feature(Lab1): добавлен класс Person с конструктором и перечислением

---
+++ Labs/Lab1/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public uint Age { get; set; }
+        public Gender Gender { get; set }
+
+        public Person(string firstName, string lastName, uint age, Gender gender) // Конструктор класса
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Gender = gender;
+        }
+    }
+}
